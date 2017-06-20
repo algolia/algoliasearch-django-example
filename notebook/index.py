88b2620491b85504f18dcfa57e8323dafc91b412
Paul-Louis NECH <pln@algolia.com>
2017-06-20 14:58:08 +0200

    chore(models): Correct indentation

--- notebook/index.py
+++ notebook/index.py
@@ -3,12 +3,12 @@ from algoliasearch_django import AlgoliaIndex
 
 class ContactIndex(AlgoliaIndex):
     fields = ('name', 'email', 'company', 'address', 'city', 'county',
-            'state', 'zip_code', 'phone', 'fax', 'web', 'followers')
+              'state', 'zip_code', 'phone', 'fax', 'web', 'followers')
 
     settings = {
         'searchableAttributes': ['name', 'email', 'company', 'city', 'county',
-            'unordered(address)', 'state', 'zip_code', 'phone', 'fax',
-            'unordered(web)'],
+                                 'unordered(address)', 'state', 'zip_code', 'phone', 'fax',
+                                 'unordered(web)'],
         'attributesForFaceting': ['city', 'company'],
         'customRanking': ['desc(followers)'],
         'queryType': 'prefixAll',
